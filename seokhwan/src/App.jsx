5759d802940a3924a253d090e37dd5ee89cd3fa0
user.email <skhu202214139@gmail.com>
2026-06-06 18:28:10 +0900

    fix: null 값에 slice 사용한 오류 수정

--- seokhwan/src/App.jsx
+++ seokhwan/src/App.jsx
@@ -4,22 +4,30 @@ import { useFetch } from "./hooks/useFetch";
 
 const App = () => {
   const { showToast } = useContext(ToastContext);
-  const { loading,result,error,errorMessage} = useFetch("/posts")
+  const { loading,data,error,errorMessage} = useFetch("/posts")
 
   useEffect (() => {
-    if (result) showToast("불러오기 성공!")
-  },[result])
-
-  useEffect (() => {
-    if (loading) showToast("로딩중")
-  },[loading])
+    if (data) showToast("불러오기 성공!")
+  },[data])
 
     useEffect (() => {
-    if (error) showToast("에라에라!")
+    if (error) showToast(errorMessage)
   },[error])
 
-  return <button onClick={() => showToast('!')}>Show Toast</button>
+  if (loading) return <p>로딩 중...</p>
 
-};
+  return (
+    <div className="grid grid-cols-2 gap-4 p-6">
+      {data?.slice(0,4).map((post) => (
+        /* aspect-square을 정사각형으로 사용했습니다 */
+        <div key={post.id} className="border rounded-lg p-4 aspect-square
+        overflow-hidden"> 
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+    </div>
+  ))}
+</div>
+  )
+}
 
 export default App;
