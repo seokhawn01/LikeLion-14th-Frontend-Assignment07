c780e8a60e1cd691c4a4e083c5735bf2c1098135
user.email <skhu202214139@gmail.com>
2026-06-06 19:05:21 +0900

    feat: 배경화면 백그라운드 컬러 변경, 게시글 hover 했을 시, 그림자 나오게 수정, 게시글 대시보드 텍스트 추가, 게시글 제목 텍스트 사이즈 키우기

--- seokhwan/src/App.jsx
+++ seokhwan/src/App.jsx
@@ -17,15 +17,19 @@ const App = () => {
   if (loading) return <p>로딩 중...</p>
 
   return (
-    <div className="grid grid-cols-2 gap-4 p-6">
+    <div className="min-h-screen bg-stone-100 p-8">
+    <h1 className="text-4xl font-bold text-stone-800 text-center mb-6">게시글 대시보드</h1>
+    <div className="max-w-2xl mx-auto grid grid-cols-2 gap-4 p-6">
       {data?.slice(0,4).map((post) => (
         /* aspect-square을 정사각형으로 사용했습니다 */
-        <div key={post.id} className="border rounded-lg p-4 aspect-square
-        overflow-hidden"> 
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
+        <div key={post.id} className="bg-white border border-stone-300
+        rounded-xl p-5 aspect-square overflow-hidden hover:shadow-xl hover:shadow-stone-300
+        transition-shadow duration-300"> 
+        <h2 className="font-bold text-stone-800 mb-2 text-1xl">{post.title}</h2>
+        <p className="text-xs text-stone-500">{post.body}</p>
     </div>
   ))}
+</div>
 </div>
   )
 }
