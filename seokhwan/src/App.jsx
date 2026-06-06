077ea09f7d805ea79f9dc5ad2b1714458ed72db3
user.email <skhu202214139@gmail.com>
2026-06-06 18:09:42 +0900

    feat: 토스트 메세지 및 axios로 게시글 목록 가져오기 구현

--- seokhwan/src/App.jsx
+++ seokhwan/src/App.jsx
@@ -1,9 +1,25 @@
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { ToastContext } from "./context/ToastContext";
+import { useFetch } from "./hooks/useFetch";
 
 const App = () => {
   const { showToast } = useContext(ToastContext);
-  return <button onClick={() => showToast('토스트 뿅!')}>Show Toast</button>
+  const { loading,result,error,errorMessage} = useFetch("/posts")
+
+  useEffect (() => {
+    if (result) showToast("불러오기 성공!")
+  },[result])
+
+  useEffect (() => {
+    if (loading) showToast("로딩중")
+  },[loading])
+
+    useEffect (() => {
+    if (error) showToast("에라에라!")
+  },[error])
+
+  return <button onClick={() => showToast('!')}>Show Toast</button>
+
 };
 
 export default App;
